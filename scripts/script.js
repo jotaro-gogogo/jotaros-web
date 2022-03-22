47adfffad8925725ffee7f1a6012ed58c9698493
Rafael Morgado Ortiz <rmorort@gmail.com>
2022-03-22 16:55:28 -0600

    New JS theme changer
    Added a theme changer through JS, but it's not a correct implementation.
    Anyways, it's late and I'm hungry, fix it at night.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-const
+function changeTheme(){
+  if(myTarget.classList.contains("dark")){
+    myTarget.classList.remove("dark");
+    myTarget.innerHTML = `<div class="moon"><i class="fa fa-moon"></i></div>`;
+    document.body.classList.remove("dark");
+    document.querySelector("header").classList.remove("dark");
+    document.querySelector("header").querySelector("span").classList.remove("dark");
+    document.querySelector("a").classList.remove("dark");
+    document.querySelector(".profile").classList.remove("dark");
+    document.querySelector(".description").classList.remove("dark");
+  } else {
+    myTarget.classList.add("dark");
+    myTarget.innerHTML = `<div class="sun"><i class="fa fa-sun"></i></div>`;
+    document.body.classList.add("dark");
+    document.querySelector("header").classList.add("dark");
+    document.querySelector("header").querySelector("span").classList.add("dark");
+    document.querySelector("a").classList.add("dark");
+    document.querySelector(".profile").classList.add("dark");
+    document.querySelector(".description").classList.add("dark");
+  }
+}
+
+const myTarget = document.querySelector(".trigger");
+myTarget.addEventListener("click", changeTheme);
